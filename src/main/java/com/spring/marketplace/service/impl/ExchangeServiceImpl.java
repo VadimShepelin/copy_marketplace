6c34a8d4e9a08ddbe0da46324ea03b9902a5236a
Вадим <kocichca@bk.ru>
2025-01-22 15:08:31 +0300

    added caching for the getProductById method of the ProductServiceImpl service

--- src/main/java/com/spring/marketplace/service/impl/ExchangeServiceImpl.java
+++ src/main/java/com/spring/marketplace/service/impl/ExchangeServiceImpl.java
@@ -2,22 +2,27 @@ package com.spring.marketplace.service.impl;
 
 import com.spring.marketplace.exception.ApplicationException;
 import com.spring.marketplace.service.ExchangeService;
+import com.spring.marketplace.service.RateService;
 import com.spring.marketplace.utils.enums.ErrorType;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
 import java.io.FileReader;
 import java.math.BigDecimal;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ExchangeServiceImpl implements ExchangeService<BigDecimal,BigDecimal> {
 
     @Value("${app.json.path}")
     private String pathToJson;
+    private final RateService rateService;
 
     @Override
     @SneakyThrows
@@ -35,4 +40,23 @@ public class ExchangeServiceImpl implements ExchangeService<BigDecimal,BigDecima
             throw new ApplicationException(ErrorType.FAILED_TO_CONVERT_CURRENCY);
         }
     }
+
+    @Override
+    @SneakyThrows
+    public BigDecimal convertCurrencyWithCache(BigDecimal object) {
+        try(Jedis jedis = new Jedis("localhost",6379)){
+            BigDecimal exchangeRate = jedis.exists("exchangeRate")?
+                    new BigDecimal(jedis.get("exchangeRate")):
+                    new BigDecimal(rateService.getExchangeRate());
+
+            log.info("convertCurrencyWithCache worked successfully");
+            return object.divide(exchangeRate, 2, BigDecimal.ROUND_HALF_UP);
+        }
+        catch (Exception exception){
+            log.error(exception.getMessage());
+            return convertCurrency(object);
+        }
+    }
+
+
 }
